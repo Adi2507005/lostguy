608375eca2ea8a541bc6b1b18d7a79a6fdc74f22
Aditya Mishra <aditya_a14@delhitechnicalcampus.ac.in>
2026-03-08 11:32:25 +0530

    Add files via upload

---
+++ lecture00/arrays.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class arrays {
+
+    public static void display(int[] arr){
+        for(int i=0;i<arr.length;i++)
+        System.out.print(arr[i]+" ");
+        System.out.println();
+
+    }
+    
+    public static void main(String[] args){
+int[] arr={5,10,15,20,25,30};
+display(arr);
+// arr[0]=15;
+// arr[1]=200;
+// arr[2]=30;
+// float[] nums=new float[6];
+// char[] s=new char[3];
+// s[0]='a';
+// s[1]='b';
+// System.out.println(s);
+// System.out.println(arr)
+// display
+
+// create a table for n
+// search m in this table
+// if found print it's index
+// int n=5;
+// int[] table= new int[10];
+// for(int i=0;i<table.length;i++){
+// table[i]=n*(i+1); 
+// }
+// int m=40;
+// for( int i=1;i<=table.length;i++){
+//     if(table[i]==m)
+//     System.out.println(i);
+// Scanner scan =new Scanner(System.in);
+// String n;
+// n=scan.nextLine();
+// System.out.println(n);
+;
+
+}
+    }
+
